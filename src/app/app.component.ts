f945e329bc89400b8b0d00c159baece53d6063b8
lester2020 <lesters821@gmail.com>
2020-12-27 22:15:23 +0800

    ngrx-basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+interface AppState {
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngrxFire';
+
+  message$: Observable<string>
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message')
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
+
+  frenchhMessage() {
+    this.store.dispatch({type: 'FRENCH'})
+  }
+
+
 }
