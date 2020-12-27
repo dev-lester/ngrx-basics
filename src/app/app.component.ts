a8fa963c1f3aa685ea768145757bbc981b758b83
lester2020 <lesters821@gmail.com>
2020-12-27 23:15:54 +0800

    ngrx basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+
 interface AppState {
-  message: string;
+  post: Post;
 }
 
 @Component({
@@ -14,19 +18,27 @@ interface AppState {
 })
 export class AppComponent {
 
-  message$: Observable<string>
+  post: Observable<Post>
+
+  text: string; // form input val
 
   constructor(private store: Store<AppState>) {
-    this.message$ = this.store.select('message')
+    this.post = this.store.select('post')
   }
 
-  spanishMessage() {
-    this.store.dispatch({type: 'SPANISH'})
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text) )
   }
 
-  frenchhMessage() {
-    this.store.dispatch({type: 'FRENCH'})
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset())
   }
 
-
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote())
+  }
+  
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote())
+  } 
 }
